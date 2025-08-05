e98e9fdda697601866115cf35ba3e9dc646d0919
FutureAI <futureai@foxmail.com>
2025-08-05 20:11:43 +0800

    补充注释

--- vllm/executor/msgspec_utils.py
+++ vllm/executor/msgspec_utils.py
@@ -6,11 +6,22 @@ from typing import Any, Type
 
 from vllm.sequence import VLLM_TOKEN_ID_ARRAY_TYPE
 
+"""这些钩子函数主要用于在 vLLM 的分布式执行环境中序列化和反序列化包含 token ID 的数组数据，确保在不同进程或节点之间高效传输序列数据。
 
+目前主要是在 Ray Distributed Executor 中使用
+"""
 def encode_hook(obj: Any) -> Any:
     """Custom msgspec enc hook that supports array types.
 
     See https://jcristharif.com/msgspec/api.html#msgspec.msgpack.Encoder
+    """
+    """自定义 msgspec enc hook: 将 Python array 对象编码为字节串 (msgspec 原生不支持 Python array)
+    
+    示例输入:
+        obj = array('l', [1, 2, 3, 4, 5])
+        
+    示例输出:
+        b'\x01\x00\x00\x00'
     """
     if isinstance(obj, array):
         assert obj.typecode == VLLM_TOKEN_ID_ARRAY_TYPE, (
@@ -24,6 +35,7 @@ def decode_hook(type: Type, obj: Any) -> Any:
 
     See https://jcristharif.com/msgspec/api.html#msgspec.msgpack.Encoder
     """
+    """自定义 msgspec dec hook: 将字节串解码回 Python array 对象, """
     if type is array:
         deserialized = array(VLLM_TOKEN_ID_ARRAY_TYPE)
         deserialized.frombytes(obj)
